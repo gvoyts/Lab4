7560b7bcdedf346b9573e4f81a98284740437ca2
gvoyts <32136149+gvoyts@users.noreply.github.com>
2017-12-13 15:22:49 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+/*
+Ganna Voytseshko
+Lab 4
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Ganna Voytseshko");
+        System.out.println("Lab 4 ");
+    }
+}
